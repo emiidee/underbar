0cec65bfec807da016e7698e16f448a1b4281dc9
Emily Dong <emily.y.dong@gmail.com>
2014-03-23 15:41:21 -0700

    invoke

--- src/underbar.js
+++ src/underbar.js
@@ -143,6 +143,19 @@ var _ = { };
   // Calls the method named by methodName on each value in the list.
   // Note: you will nead to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    var results =[];
+
+    if(typeof(functionOrKey) == 'function'){
+      for (var i = 0; i < collection.length; i++) {
+        results.push(functionOrKey.apply(collection[i], args));
+      };
+    } else {
+      for (var i = 0; i < collection.length; i++) {
+        results.push(collection[i][functionOrKey].apply(collection[i], args));
+      };
+    };
+    
+    return results;
   };
 
   // Reduces an array or object to a single value by repetitively calling
