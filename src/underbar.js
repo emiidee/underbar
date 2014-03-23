40a87dd6e7764706dda398045b28fc19f8a06b3e
Emily Dong <emily.y.dong@gmail.com>
2014-03-23 12:10:50 -0700

    reject

--- src/underbar.js
+++ src/underbar.js
@@ -90,6 +90,10 @@ var _ = { };
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var reverse = function(x) {
+      return !test(x);
+    };
+    return _.filter(collection, reverse);
   };
 
   // Produce a duplicate-free version of the array.
