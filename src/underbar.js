e37fb4acce0e1e9b5e5a193630fce1679c3cca33
Emily Dong <emily.y.dong@gmail.com>
2014-03-23 20:45:57 -0700

    some

--- src/underbar.js
+++ src/underbar.js
@@ -215,6 +215,9 @@ var _ = { };
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    return !_.every(collection, function(value){
+      return !iterator(value);
+    });
   };
 
 
